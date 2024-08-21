49102d57cfdf9c1f901514039c83a699a1c5f9a8
joeychen <joeychen@odd-e.com>
2024-08-21 20:56:25 +0800

    rename and introduce variable: overlapping_amount

--- budget_service.py
+++ budget_service.py
@@ -34,18 +34,17 @@ class BudgetService:
         end_month = datetime.date(end.year, end.month, 1) + relativedelta(months=+1)
         while current_date < end_month:
             if current_date.strftime('%Y%m') == start.strftime('%Y%m'):
-                total_amount_of_start = self.get_single_day_amount(start.year, start.month) * (
+                overlapping_amount = self.get_single_day_amount(start.year, start.month) * (
                         self.get_days_in_month(start.year, start.month) - start.day + 1)
-                total_amount += total_amount_of_start
+                total_amount += overlapping_amount
             elif current_date.strftime('Y%m') == end.strftime('Y%m'):
-                total_amount_of_end = self.get_single_day_amount(end.year, end.month) * end.day
-                total_amount += total_amount_of_end
-
+                overlapping_amount = self.get_single_day_amount(end.year, end.month) * end.day
+                total_amount += overlapping_amount
             else:
-                total_amount += self.get_single_day_amount(current_date.year,
-                                                           current_date.month) * self.get_days_in_month(
-                    current_date.year,
-                    current_date.month)
+                overlapping_amount = self.get_single_day_amount(current_date.year,
+                                                                current_date.month) * self.get_days_in_month(
+                    current_date.year, current_date.month)
+                total_amount += overlapping_amount
             current_date += relativedelta(months=1)
 
         return total_amount
