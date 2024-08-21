6ad281032ad32e957652a15c07fe053035725d33
joeychen <joeychen@odd-e.com>
2024-08-21 20:56:59 +0800

    move duplicated of total_amount calculation out of if/else block

--- budget_service.py
+++ budget_service.py
@@ -36,15 +36,16 @@ class BudgetService:
             if current_date.strftime('%Y%m') == start.strftime('%Y%m'):
                 overlapping_amount = self.get_single_day_amount(start.year, start.month) * (
                         self.get_days_in_month(start.year, start.month) - start.day + 1)
-                total_amount += overlapping_amount
+                # total_amount += overlapping_amount
             elif current_date.strftime('Y%m') == end.strftime('Y%m'):
                 overlapping_amount = self.get_single_day_amount(end.year, end.month) * end.day
-                total_amount += overlapping_amount
+                # total_amount += overlapping_amount
             else:
                 overlapping_amount = self.get_single_day_amount(current_date.year,
                                                                 current_date.month) * self.get_days_in_month(
                     current_date.year, current_date.month)
-                total_amount += overlapping_amount
+                # total_amount += overlapping_amount
+            total_amount += overlapping_amount
             current_date += relativedelta(months=1)
 
         return total_amount
