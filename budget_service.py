bfa7eeb891a599bf16b7134be2491bc0d50d135e
joeychen <joeychen@odd-e.com>
2024-08-21 20:50:22 +0800

    introduce variables: total_amount_of_start and total_amount_of_end

--- budget_service.py
+++ budget_service.py
@@ -26,9 +26,11 @@ class BudgetService:
         if start.year == end.year and start.month == end.month:
             return self.get_single_day_amount(start.year, start.month) * (end.day - start.day + 1)
 
-        total_amount = self.get_single_day_amount(start.year, start.month) * (
-                self.get_days_in_month(start.year, start.month) - start.day + 1) + self.get_single_day_amount(end.year,
-                                                                                                              end.month) * end.day
+        total_amount_of_start = self.get_single_day_amount(start.year, start.month) * (
+                self.get_days_in_month(start.year, start.month) - start.day + 1)
+        total_amount_of_end = self.get_single_day_amount(end.year, end.month) * end.day
+
+        total_amount = total_amount_of_start + total_amount_of_end
         current_date = start + relativedelta(months=1)
         end_month = datetime.date(end.year, end.month, 1)
         while current_date < end_month:
