82094fec6fa8812f54b1e2c06479b32204df8e95
joeychen <joeychen@odd-e.com>
2024-08-21 20:53:50 +0800

    move total_amount_of_start into while loop, and change initial value of current_date to start

--- budget_service.py
+++ budget_service.py
@@ -26,17 +26,25 @@ class BudgetService:
         if start.year == end.year and start.month == end.month:
             return self.get_single_day_amount(start.year, start.month) * (end.day - start.day + 1)
 
-        total_amount_of_start = self.get_single_day_amount(start.year, start.month) * (
-                self.get_days_in_month(start.year, start.month) - start.day + 1)
+        # total_amount_of_start = self.get_single_day_amount(start.year, start.month) * (
+        #         self.get_days_in_month(start.year, start.month) - start.day + 1)
         total_amount_of_end = self.get_single_day_amount(end.year, end.month) * end.day
 
-        total_amount = total_amount_of_start + total_amount_of_end
-        current_date = start + relativedelta(months=1)
+        total_amount = total_amount_of_end
+        # total_amount = total_amount_of_start + total_amount_of_end
+        current_date = start
+        # current_date = start + relativedelta(months=1)
         end_month = datetime.date(end.year, end.month, 1)
         while current_date < end_month:
-            total_amount += self.get_single_day_amount(current_date.year, current_date.month) * self.get_days_in_month(
-                current_date.year,
-                current_date.month)
+            if current_date.strftime('%Y%m') == start.strftime('%Y%m'):
+                total_amount_of_start = self.get_single_day_amount(start.year, start.month) * (
+                        self.get_days_in_month(start.year, start.month) - start.day + 1)
+                total_amount += total_amount_of_start
+            else:
+                total_amount += self.get_single_day_amount(current_date.year,
+                                                           current_date.month) * self.get_days_in_month(
+                    current_date.year,
+                    current_date.month)
             current_date += relativedelta(months=1)
 
         return total_amount
